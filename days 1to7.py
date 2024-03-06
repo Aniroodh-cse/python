4d0fc9e5007a6990847e7a979e72bd2d677fae0d
Aniroodh <151647723+Aniroodh-cse@users.noreply.github.com>
2024-03-06 15:43:38 +0530

    Create days 1to7.py

---
+++ days 1to7.py
@@ -0,0 +1,17 @@
+days=int(input("Enter the number of days for 1 to 7 :"))
+if days==1:
+  print("Monday")
+elif days==2:
+  print("Tuesday")
+elif days==3:
+  print("Wednesday")
+elif days==4:
+  print("Thursday")
+elif days==5:
+  print("Friday")
+elif days==6:
+  print("Saturday")
+elif days==7:
+  print("Sunday")
+else:
+  print("Invalid input")
