271b16c76c997f80cbe55912ee453bd65d532eb9
Aniroodh <151647723+Aniroodh-cse@users.noreply.github.com>
2024-03-06 15:45:02 +0530

    Create root of Quadratic equation.py

---
+++ root of Quadratic equation.py
@@ -0,0 +1,7 @@
+print("This is the formula :ax^2 + bx + c = 0")
+a=int(input("Enter the a value :"))
+b=int(input("Enter the b value :"))
+x=int(input("Enter the x value :"))
+c=int(input("Enter the c value :"))
+y=a*x*x+b*x+c
+print("The value of y is :",y)
